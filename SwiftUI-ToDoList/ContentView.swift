dfe259a0cce6d7d3bb5b14db17d8c04ce3291661
dave17code <dave17code@gmail.com>
2024-09-06 09:11:53 +0900

    Initial Commit

---
+++ SwiftUI-ToDoList/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI-ToDoList
+//
+//  Created by 김두운 on 9/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI-ToDoList/SwiftUI_ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ToDoListApp.swift
+//  SwiftUI-ToDoList
+//
+//  Created by 김두운 on 9/6/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
